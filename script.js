26cb56fc73aa637f00250ea82c6a300384821f8b
BartekM <160483027+mazurek321@users.noreply.github.com>
2024-10-02 09:58:29 +0000

    drugi

--- script.js
+++ script.js
@@ -6,9 +6,17 @@
   button.addEventListener("click", () => {
     content.innerHTML = "0,1,2,3,4,5,6,7,8,9";
   });
- const input = document.getElementById("ex2_text").value;
-  input.addEventListener("onchange", () => {
-    input.lenght != 9 ? content2.innerHTML = "error" : content2.innerHTML = "ok";;
+ const input = document.getElementById("ex2_text");
+  input.addEventListener("input", () => {
+    content2.innerText = "";
+    if(input.value.lenght != 9)
+    {
+      content2.innerHTML = "Długość numeru musi być równa 9";
+    }
+    if(input.value.lenght == 9)
+      content2.innerHTML = "ok";
+    if(input.value.match(/[a-z]/i))
+      content2.innerHTML += "Numer nie może zawierać liter";
   })
   
 })();

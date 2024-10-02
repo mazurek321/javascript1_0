960cc9e484c489a08bc82081f1cad0f07d30d909
bartek mazurkiewicz <36937471-mazurek32193@users.noreply.replit.com>
2024-10-02 09:33:44 +0000

    pierwszy

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 (function () {
-  //TODO
+  const button = document.getElementById("ex1_button");
+  const content = document.getElementById("ex1_content");
+  const content2 = document.getElementById("ex2_content");
+
+  button.addEventListener("click", () => {
+    content.innerHTML = "0,1,2,3,4,5,6,7,8,9";
+  });
+ const input = document.getElementById("ex2_text").value;
+  input.addEventListener("onchange", () => {
+    input.lenght != 9 ? content2.innerHTML = "error" : content2.innerHTML = "ok";;
+  })
+  
 })();
